f1d30c65b697ef27c920e9e08e1a285689d9b6b0
Mahesh <85110955+m9hesh@users.noreply.github.com>
2023-08-18 12:27:19 +0530

    Create threebig.c
    Finding big number in 3

---
+++ threebig.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+
+    int a,b,c;
+    scanf("%d%d%d", &a,&b,&c);
+    if(a>b){
+        if(a>c){
+         printf("a is bigger");
+        }else{
+            printf("c is bigger");
+        }
+    }else if(b>c){
+        printf("b is bigger");
+    }else{
+        printf("c is bigger");
+    }
+}
